67de73936f88dce0c96ffaa4edc35ba0e8ade700
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-04-22 17:47:32 -0700

    adding firebase js under recieved.js

---
+++ js/received.js
@@ -0,0 +1,17 @@
+function recievedData(data) {
+  // need to retrieve firebase data with val() method
+  // this returns an object of all data
+  fbData = data.val();
+  if (fbData) {
+    // check to see if there is something in your database to start
+    console.log('received data:');
+    console.log(fbData);
+
+    // create an array of the post values (if you need to loop through it retaining order of entries)
+    fbDataArray = Object.values(fbData);
+  } else {
+    console.log('nothing in this folder yet');
+  }
+}
+
+function errData(err) {}
