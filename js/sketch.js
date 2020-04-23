adb30f62e5065892877d17aa11dd02d9c1a44bdc
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-04-22 17:24:42 -0700

    adding data base

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  // database page
+  // https://console.firebase.google.com/u/1/project/messageinabottle-4ee9f/database/messageinabottle-4ee9f/data
+  // Your web app's Firebase configuration
+  // const firebaseConfig = {
+  //   apiKey: 'AIzaSyDCgdmBxjoYzsqKOj6F8p9Lxry3Iu20oKk',
+  //   authDomain: 'messageinabottle-4ee9f.firebaseapp.com',
+  //   databaseURL: 'https://messageinabottle-4ee9f.firebaseio.com',
+  //   projectId: 'messageinabottle-4ee9f',
+  //   storageBucket: 'messageinabottle-4ee9f.appspot.com',
+  //   messagingSenderId: '1029081416271',
+  //   appId: '1:1029081416271:web:7c4f60842968a01527e03a',
+  // };
 }
 
-function draw() {
-  background(220);
-}
+function draw() {}
