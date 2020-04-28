3eb9786c5f5232aa59b8c832410edabb726aad35
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-04-27 20:21:50 -0700

    adjusting mq

--- js/received.js
+++ js/received.js
@@ -1,25 +1,71 @@
-// /* eslint-disable */
+// DOCS: https://github.com/larkbuck/xsocial/blob/master/README.md
+
+let nodeData; // obeject we will push to firebase
+let fbData; // data we pull from firebase
+let fbDataArray; // firebase data values converted to an Array
+let database; // reference to our firebase database
+const folderName = 'messages'; // name of folder you create in db
+const form = document.querySelector('form.send');
+const receiveButton = document.querySelector('button.receive');
+function start() {
+  // Initialize firebase
+  // support for Firebase Realtime Database 4 web here: https://firebase.google.com/docs/database/web/start
+
+  // paste your config file here
+  const configureKey = {
+    apiKey: 'AIzaSyDCgdmBxjoYzsqKOj6F8p9Lxry3Iu20oKk',
+    authDomain: 'messageinabottle-4ee9f.firebaseapp.com',
+    databaseURL: 'https://messageinabottle-4ee9f.firebaseio.com',
+    projectId: 'messageinabottle-4ee9f',
+    storageBucket: 'messageinabottle-4ee9f.appspot.com',
+    messagingSenderId: '1029081416271',
+    appId: '1:1029081416271:web:7c4f60842968a01527e03a',
+  };
+
+  // initialize cofig
+  firebase.initializeApp(configureKey);
+  // store database
+  database = firebase.database();
+
+  // this references the folder you want your data to appear in
+  const ref = database.ref(folderName);
+  // **** folderName must be consistent across all calls to this folder
+
+  ref.on('value', recievedData, errData);
+
+  // ---> To find your config object:
+  // They will provide it during Firebase setup
+  // or (if your project already created)
+  // 1. Go to main console page
+  // 2. Click on project
+  // 3. On project home page click on name of app under project name (in large font)
+  // 4. Click the gear icon --> it's in there!
+
+  // database page
+  // https://console.firebase.google.com/u/1/project/messageinabottle-4ee9f/database/messageinabottle-4ee9f/data
+  // Your web app's Firebase configuration
+}
+start();
+
 function recievedData(data) {
   // need to retrieve firebase data with val() method
   // this returns an object of all data
   fbData = data.val();
 
-  // fbData
-  //   ? (fbDataArray =
-  //       Object.values(fbData) && console.log('received data:', fbData))
-  //   : console.log('nothing in this folder yet');
-  if (fbData) {
-    // check to see if there is something in your database to start
-    // console.log('received data:', fbData);
+  fbData
+    ? (fbDataArray = Object.values(fbData))
+    : console.log('nothing in this folder yet');
+  // if (fbData) {
+  //   // check to see if there is something in your database to start
+  //   // console.log('received data:', fbData);
 
-    // create an array of the post values (if you need to loop through it retaining order of entries)
-    fbDataArray = Object.values(fbData);
-  } else {
-    console.log('nothing in this folder yet');
-  }
+  //   // create an array of the post values (if you need to loop through it retaining order of entries)
+  //   fbDataArray = Object.values(fbData);
+  // } else {
+  //   console.log('nothing in this folder yet');
+  // }
 }
 
-console.log(fbDataArray);
 function errData(err) {
   console.log('error! did not receive data');
   console.log(err);
@@ -33,40 +79,6 @@ function createNode(_nodeFolder, _nodeId, _nodeObject) {
     .ref(`${_nodeFolder}/${_nodeId}`)
     .set(_nodeObject);
 }
-
-const form = document.querySelector('form.send');
-const receiveButton = document.querySelector('button.receive');
-
-function sendMessage(e) {
-  // e.preventDefault();
-  const messageInput = form.querySelector('textarea[name="message"]');
-  const timeStamp = Date.now();
-  console.dir(timeStamp);
-
-  const validChars = char => char.match(/[a-z1-9 .\n]/i);
-
-  const results = [...messageInput.value].filter(validChars).join('');
-
-  console.log(results);
-
-  nodeData = { message: messageInput.value, timeStamp };
-
-  createNode(folderName, timeStamp, nodeData);
-
-  alert('Message Sent');
-}
-
-function receiveMessage(e) {
-  e.preventDefault();
-  const randomNumber = Math.floor(Math.random() * fbDataArray.length);
-  const pTag = document.querySelector('.receivedData');
-  console.log(randomNumber);
-
-  pTag.textContent = fbDataArray[randomNumber].message;
-}
-
-form.addEventListener('submit', sendMessage);
-receiveButton.addEventListener('click', receiveMessage);
 
 // call this function in the web console to create and seed the folder!
 // createNode(folderName, "seed", {text: "this is to seed folder"});
@@ -94,3 +106,28 @@ function deleteNode(_nodeFolder, _nodeID) {
     .ref(`${_nodeFolder}/${_nodeID}`)
     .remove();
 }
+
+function sendMessage(e) {
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const timeStamp = Date.now();
+  // const validChars = char => char.match(/[a-z1-9 .\n]/i);
+  // const results = [...messageInput.value].filter(validChars).join('');
+
+  nodeData = { message: messageInput.value, timeStamp };
+
+  createNode(folderName, timeStamp, nodeData);
+
+  alert(
+    'Thank you! Your message was received. Take care of yourself the best way you can :)'
+  );
+}
+
+function receiveMessage(e) {
+  e.preventDefault();
+  const randomNumber = Math.floor(Math.random() * fbDataArray.length);
+  const pTag = document.querySelector('.receivedData');
+  pTag.textContent = fbDataArray[randomNumber].message;
+}
+
+form.addEventListener('submit', sendMessage);
+receiveButton.addEventListener('click', receiveMessage);
