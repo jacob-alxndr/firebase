028b6f275653da8aa0d0221bda0ec98498045a23
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-05-10 14:16:30 -0700

    adding micro interactions on buttons and inputs

--- src/js/api.js
+++
@@ -1,9 +0,0 @@
-// const apiURL = 'https://ign-apis.herokuapp.com/';
-
-// async function getIGN() {
-//   const response = await fetch(apiURL);
-//   const data = await response.json();
-//   console.log(data);
-// }
-
-// getIGN();
